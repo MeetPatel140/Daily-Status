32b6e75850492ea8e31c71bae08849f334a5ba73
MeetPatel140 <meetsolanki140@gmail.com>
2024-02-29 18:27:23 +0530

    [29 Feb] [main] Code Safe

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module DailyStatusWebapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Kolkata"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
